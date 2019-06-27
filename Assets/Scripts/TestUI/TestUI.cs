8958baf70aaf4739f4759d8cde23a0c644278e3e
Rafiki <raphaelle.mp@gmail.com>
2019-06-27 17:05:31 -0400

    Trying out thing for UI, halp

---
+++ Assets/Scripts/TestUI/TestUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUI : MonoBehaviour
+{
+    public float speed;
+    public bool isChanging;
+
+    void Update() {
+        transform.RotateAround(transform.position, Vector3.forward, speed * Time.deltaTime);
+
+        /*if (Quaternion.Angle(transform.rotation, closingRotation.transform.rotation) <= 0.01f && !opening) {
+            print("ChangeSpeed");
+            speed *= -1;
+            opening = !opening;
+        }*/
+    }
+}

58c92a63c91d3154ec35d04b63e9de017924aed5
Rafiki <raphaelle.mp@gmail.com>
2019-07-02 13:27:17 -0400

    Boulders reappearing bug fixed (only reappear after rewinding level)

--- Assets/Scripts/Objects/Boulder.cs
+++ Assets/Scripts/Objects/Boulder.cs
@@ -3,35 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boulder : MonoBehaviour {
+    ObjectsManager objectManagerScript;
 	Protagonist protagonistScript;
 	Renderer[] rend;
-	bool enemyIsDead = false;
+	bool boulderIsDestroyed = false;
+
+
 
 	void Awake(){
 		protagonistScript = GameObject.Find ("Protagonist").GetComponent<Protagonist>();
 		rend = gameObject.GetComponentsInChildren<Renderer> ();
+        objectManagerScript = GameObject.FindGameObjectWithTag("Manager").GetComponent<ObjectsManager>();
 	}
 
 	void OnTriggerEnter(Collider coll){
 		string gameObjectName = coll.gameObject.name;
-		if (gameObjectName == "Protagonist" && !enemyIsDead) {
-			gameOver ();
-		}
-		else if(gameObjectName.Contains("Bullet")){
-			Destroy (coll.gameObject);
-			for (int i = 0; i < rend.Length; i++) {
-				rend [i].enabled = false;
-			}
-			enemyIsDead = true;
-			Invoke("makeVisible", 1.5f);
-		}
+        if (gameObjectName == "Protagonist" && !boulderIsDestroyed) {
+            gameOver();
+        }
+        else if (gameObjectName.Contains("Bullet") && !boulderIsDestroyed) {
+            Destroy(coll.gameObject);
+            for (int i = 0; i < rend.Length; i++) {
+                objectManagerScript.destroyedBoulders.Add(gameObject);
+                rend[i].enabled = false;
+            }
+            boulderIsDestroyed = true;
+        }
+        else if (coll.gameObject.tag == "RendBorder" && protagonistScript.respawning) {
+            makeVisible();
+        }
 	}
 
 	void makeVisible() {
 		for (int i = 0; i < rend.Length; i++) {
 			rend [i].enabled = true;
 		}
-		enemyIsDead = false;
+		boulderIsDestroyed = false;
 	}
 
 	void gameOver(){

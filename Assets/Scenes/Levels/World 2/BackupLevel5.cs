56ced00c5aba84ebb5f000a3fe4a20d9ac55e8ca
Rafiki <raphaelle.mp@gmail.com>
2019-07-05 09:08:58 -0400

    C'est quoi ca

---
+++ Assets/Scenes/Levels/World 2/BackupLevel5.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackupLevel5 : MonoBehaviour
+{
+    /*
+     s..,w..,b..s..,w..,b..3p..w.;b.;2p.;,w.;b..c
+...3p..,w...;9p.;w..b..;w..b.. s.;w..b..s..;b..;w.;b.;c
+..s....2p...;b..s...p....;,b..s..,w..,b...;b
+         
+         */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
